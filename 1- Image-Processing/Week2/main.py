6e16539858672ba2ed7b3ab70c3d9a3f0a653f41
ebubekir doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-10-15 19:27:01 +0300

    update week 2

--- 1- Image-Processing/Week2/main.py
+++ 1- Image-Processing/Week2/main.py
@@ -12,21 +12,17 @@ print("R değeri " ,img[0,0,2])  # 2 olan R değeri   satır x sutun x RGB deger
 print("G değeri " ,img[0,0,1])  # 1 olan G değeri
 print("B değeri " ,img[0,0,0])  # 0 olan B degeri
 
-
 baslangic_satiri = 400
 bitis_satiri = 430
 
 baslangic_sutunu =200
 bitis_sutunu = 320
 
-
 img_kesilen = img[baslangic_satiri:bitis_satiri,baslangic_sutunu:bitis_sutunu]
-
 
 #cv2.imshow("kesilen resim",img_kesilen)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-
 
 img_kesilen_R = img_kesilen[:,:,2]
 img_kesilen_G = img_kesilen[:,:,1]
@@ -42,5 +38,4 @@ img_grayscale = cv2.imread(img_path,0)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
 print(img_grayscale.shape)
-
 print(img_grayscale[0,0])
