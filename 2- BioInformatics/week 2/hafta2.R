a043a92e5d12ca29273c09f90f4e6892b28de7c7
ebubekir doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-10-16 11:34:26 +0300

    create

---
+++ 2- BioInformatics/week 2/hafta2.R
@@ -0,0 +1,9 @@
+# GPL,GSM,GSE ve GDS 
+
+#GPL ; platform kaydı,platform üretici,teknoloji türü, platform tanımı gibi bilgileri barındırır.
+#GSM ; tek bir örnekle ilgili bilgileri bünyesinde barındırmaktadır.
+#GSE ; veri seti kaydı olarak ifade edilir.Tüm örneklerin veri kümesi olarak ifade edilir.
+#GDS ; veri kayıtlarının ve platform kayıtlarının içerdiği bilgilerden oluşan kümedir.
+#GEO ; verileri çekmek içim  BiocManager::(install) kütüphanesi kurulmalıdır.
+  
+ 
