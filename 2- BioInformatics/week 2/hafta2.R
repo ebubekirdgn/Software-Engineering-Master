53ec47bb951fa291e14e383030546ef67670fadd
ebubekir doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-10-16 11:41:59 +0300

    bladder_cancer

--- 2- BioInformatics/week 2/hafta2.R
+++ 2- BioInformatics/week 2/hafta2.R
@@ -6,4 +6,8 @@
 #GDS ; veri kayıtlarının ve platform kayıtlarının içerdiği bilgilerden oluşan kümedir.
 #GEO ; verileri çekmek içim  BiocManager::(install) kütüphanesi kurulmalıdır.
   
- 
+library(GEOquery)
+bladder_cancer = getGEO("GDS183")
+
+#GetGEO ; fonksiyonu söz konusu veri kümesini bilgisayarda gecici olarak bir alana yerleştirmekte ve aynı 
+# zamanda bladder_cancer isimli bir değişken oluşturulmasına imkan sağlamaktadır.
