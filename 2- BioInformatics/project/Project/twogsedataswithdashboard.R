14631709ad6e3d58c6bb8549c59676499124fcf8
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-12-26 17:06:01 +0300

    nsfilter is working latest version

--- 2- BioInformatics/project/Project/twogsedataswithdashboard.R
+++ 2- BioInformatics/project/Project/twogsedataswithdashboard.R
@@ -1,13 +1,22 @@
+# Gerekli kütüphaneleri yükle
+if (!require("shiny")) install.packages("shiny")
+if (!require("GEOquery")) install.packages("GEOquery")
+if (!require("shinydashboard")) install.packages("shinydashboard")
+if (!require("plotly")) install.packages("plotly")
+if (!require("DT")) install.packages("DT")
+
 library(shiny)
 library(GEOquery)
 library(shinydashboard)
 library(plotly)
 library(DT)
+library(genefilter)
 
 getGSE <- function(gseNumber) {
   gse <- getGEO(gseNumber, GSEMatrix = TRUE, AnnotGPL = TRUE)
   exprs <- exprs(gse[[1]])
   exprs1 <- (gse[[1]])
+  
   return(list(exprs = exprs, exprs1 = exprs1))
 }
 
@@ -15,23 +24,26 @@ getGSE <- function(gseNumber) {
 ui <- dashboardPage(
   dashboardHeader(title = "GSE Veri İşleme"),
   dashboardSidebar(
-    textInput("gse1", "İlk GSE Veri Numarası:", value = ""),
-    textInput("gse2", "İkinci GSE Veri Numarası:", value = ""),
+    textInput("gse1", "İlk GSE Veri Numarası:", value = "GSE1010"),
+    textInput("gse2", "İkinci GSE Veri Numarası:", value = "GSE1009"),
     selectInput("columnSelect1", "Sütun Seç 1:", choices = NULL),
     selectInput("columnSelect2", "Sütun Seç 2:", choices = NULL),
     actionButton("submitBtn", "Verileri Getir"),
     actionButton("StunBtn", "Pdata Getir"),
+    actionButton("cleanNABtn", "Clean NA Data"),
     actionButton("mergeBtn", "Merge Cleaned Data"),
-    actionButton("pdataMergeBtn", "PData Merge")  # PData Merge butonu eklendi
+    actionButton("filterBtn", "Filtrele")  # Yeni eklenen Filtrele butonu
   ),
   dashboardBody(
-    box(title = "İlk GSE Verisi", width = 6, solidHeader = TRUE, DTOutput("gse1Table")),
-    box(title = "İkinci GSE Verisi", width = 6, solidHeader = TRUE, DTOutput("gse2Table")),
-    box(title = "Pdata veri 1", width = 6, solidHeader = TRUE, DTOutput("PdataTable")),
-    box(title = "Pdata veri 2", width = 6, solidHeader = TRUE, DTOutput("PdataTable1")),
-    box(title = "Birleştirilmiş Veri", width = 6, solidHeader = TRUE, DTOutput("mergedTable")),
-    box(title = "Birleştirilmiş Pdata", width = 6, solidHeader = TRUE, DTOutput("mergedPDataTable")), # Ekran için yeni bir tablo eklendi
+    box(title = "İlk GSE Verisi", width = 6, solidHeader = TRUE, tableOutput("gse1Table")),
+    box(title = "İkinci GSE Verisi", width = 6, solidHeader = TRUE, tableOutput("gse2Table")),
+    box(title = "Temizlenmiş Veri", width = 6, solidHeader = TRUE, tableOutput("cleanedTable")),
+    box(title = "Pdata veri", width = 6, solidHeader = TRUE, DTOutput("PdataTable")),
+    box(title = "Pdata veri", width = 6, solidHeader = TRUE, DTOutput("PdataTable1")),
+    box(title = "Birleştirilmiş Veri", width = 6, solidHeader = TRUE, tableOutput("mergedTable")),
     box(title = "Görselleştirme", width = 12, solidHeader = TRUE, plotlyOutput("mergedPlot")),
+    box(title = "Filtre GSE1", width = 6, solidHeader = TRUE, DTOutput("filteredGSE1Table")),  # Yeni eklenen Filtre GSE1 tablosu
+    box(title = "Filtre GSE2", width = 6, solidHeader = TRUE, DTOutput("filteredGSE2Table"))   # Yeni eklenen Filtre GSE2 tablosu
   )
 )
 
@@ -46,111 +58,79 @@ server <- function(input, output, session) {
   
   # Veri alma butonu için tepki
   observeEvent(input$submitBtn, {
-    tryCatch({
-      gse1Data <- getGSE(input$gse1)
-      gse2Data <- getGSE(input$gse2)
-      
-      colnames_pdata <- colnames(gse1Data$exprs1@phenoData@data)
-      colnames_pdata1 <- colnames(gse2Data$exprs1@phenoData@data)
-      updateSelectInput(session, "columnSelect1", choices = colnames_pdata)
-      updateSelectInput(session, "columnSelect2", choices = colnames_pdata1)
-      
-      output$gse1Table <- renderDT({
-        datatable(head(gse1Data$exprs), options = list(pageLength = 10))
-      })
-      
-      output$gse2Table <- renderDT({
-        datatable(head(gse2Data$exprs), options = list(pageLength = 10))
-      })
-    }, error = function(e) {
-      showModal(
-        modalDialog(
-          title = "Hata",
-          "Veri alma sırasında bir hata oluştu. Lütfen bağlantınızı kontrol edin ve tekrar deneyin.",
-          easyClose = TRUE,
-          footer = NULL
-        )
-      )
-      shinyjs::reset("errorButton")
-      shinyjs::enable("errorButton")
-      shinyjs::runjs('Shiny.onInputChange("error", true);')
-    })
-  })
-  
-  observeEvent(input$StunBtn, {
+    # GSE verilerini al
     gse1Data <- getGSE(input$gse1)
     gse2Data <- getGSE(input$gse2)
-    selected_column1 <- input$columnSelect1
-    selected_column2 <- input$columnSelect2
-    output$PdataTable <- renderDT({
-      datatable(as.data.frame(gse1Data$exprs1@phenoData@data[, selected_column1]), options = list(scrollX = TRUE, scrollY = TRUE, pageLength = 10))
+    
+    colnames_pdata <- colnames(gse1Data$exprs1@phenoData@data)
+    colnames_pdata1 <- colnames(gse2Data$exprs1@phenoData@data)
+    updateSelectInput(session, "columnSelect1", choices = colnames_pdata)
+    updateSelectInput(session, "columnSelect2", choices = colnames_pdata1)
+    
+    # İlk GSE verisini ekranda göster
+    output$gse1Table <- renderTable({
+      head(gse1Data$exprs)  # Veri çerçevesini direkt göster
     })
-    filtre1 = gse1Data$exprs1@phenoData@data[, selected_column1]
-    output$PdataTable1 <- renderDT({
-      datatable(as.data.frame(gse2Data$exprs1@phenoData@data[, selected_column2]), options = list(scrollX = TRUE, scrollY = TRUE, pageLength = 10))
+    
+    # İkinci GSE verisini ekranda göster
+    output$gse2Table <- renderTable({
+      head(gse2Data$exprs)  # Veri çerçevesini direkt göster
     })
-    filtre2 = gse2Data$exprs1@phenoData@data[, selected_column2]
   })
   
-  observeEvent(input$pdataMergeBtn, {
+  # Filtrele butonu için tepki
+  observeEvent(input$filterBtn, {
     gse1Data <- getGSEData()$gse1Data
     gse2Data <- getGSEData()$gse2Data
-    selected_column1 <- input$columnSelect1
-    selected_column2 <- input$columnSelect2
     
-    pdata1 <- gse1Data$exprs1@phenoData@data[, selected_column1]
-    pdata2 <- gse2Data$exprs1@phenoData@data[, selected_column2]
-    
-    # PData1 ve PData2'yi alt alta birleştir
-    mergedPData <- rbind(
-      data.frame(PData = pdata1),
-      data.frame(PData = pdata2)
+    # GSE verilerini nsFilter işlemine tabi tut
+    eset1 <- gse1Data$exprs1
+    annotation(eset1) <- "hgu133plus2.db"
+    filtrelenmis1 <- nsFilter(
+      eset1,
+      require.entrez = TRUE,
+      require.GOBP = FALSE,
+      require.GOCC = FALSE,
+      require.GOMF = FALSE,
+      require.CytoBand = FALSE,
+      remove.dupEntrez = TRUE,
+      var.func = IQR,
+      var.cutoff = 0.90,
+      var.filter = TRUE,
+      filterByQuantile = TRUE,
+      feature.exclude = "^AFFX"
     )
+    sonveri1 <- data.frame(t(exprs(filtrelenmis1$eset)))
     
-    # Birleştirilmiş PData'yı yazdır
-    output$mergedPDataTable <- renderDT({
-      datatable(mergedPData, options = list(scrollX = TRUE, scrollY = TRUE, pageLength = 10))
-    })
-  })
-  
-  observeEvent(input$mergeBtn, {
-    cleanedData1 <- getGSEData()$gse1Data$exprs[complete.cases(getGSEData()$gse1Data$exprs), ]
-    cleanedData2 <- getGSEData()$gse2Data$exprs[complete.cases(getGSEData()$gse2Data$exprs), ]
-    
-    mergedData <- dplyr::bind_rows(
-      data.frame(ID = rownames(cleanedData1), cleanedData1),
-      data.frame(ID = rownames(cleanedData2), cleanedData2),
-      .id = "Source"
+    eset2 <- gse2Data$exprs1
+    annotation(eset2) <- "hgu133plus2.db"
+    filtrelenmis2 <- nsFilter(
+      eset2,
+      require.entrez = TRUE,
+      require.GOBP = FALSE,
+      require.GOCC = FALSE,
+      require.GOMF = FALSE,
+      require.CytoBand = FALSE,
+      remove.dupEntrez = TRUE,
+      var.func = IQR,
+      var.cutoff = 0.90,
+      var.filter = TRUE,
+      filterByQuantile = TRUE,
+      feature.exclude = "^AFFX"
     )
+    sonveri2 <- data.frame(t(exprs(filtrelenmis2$eset)))
     
-    output$mergedTable <- renderDT({
-      datatable(head(mergedData), options = list(pageLength = 10))
+    # Filtrelenmiş GSE verilerini ekranda göster
+    output$filteredGSE1Table <- renderDT({
+      datatable(sonveri1, options = list(scrollX = TRUE, scrollY = TRUE))
     })
     
-    output$mergedPlot <- renderPlotly({
-      plot_ly(data = mergedData, x = ~ID, y = ~`ID`, type = 'scatter', mode = 'lines+markers', color = ~Source)
+    output$filteredGSE2Table <- renderDT({
+      datatable(sonveri2, options = list(scrollX = TRUE, scrollY = TRUE))
     })
   })
   
-  observe({
-    shinyjs::enable("errorButton")
-  })
-  
-  observeEvent(input$error, {
-    if (input$error) {
-      showModal(
-        modalDialog(
-          title = "Hata",
-          "Veri alma sırasında bir hata oluştu. Lütfen bağlantınızı kontrol edin ve tekrar deneyin.",
-          easyClose = TRUE,
-          footer = NULL
-        )
-      )
-      shinyjs::reset("errorButton")
-      shinyjs::enable("errorButton")
-      shinyjs::runjs('Shiny.onInputChange("error", false);')
-    }
-  })
+  # ... (Diğer kod parçacıkları)
 }
 
 # Uygulamayı çalıştır
